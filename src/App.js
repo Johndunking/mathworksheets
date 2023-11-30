8a06ae64b224289ea1fb5ffb878971d607c95a84
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-11-29 17:44:57 -0800

    reverted back to bootstrap

--- src/App.js
+++ src/App.js
@@ -123,22 +123,23 @@ function App() {
 
       {!problemsGenerated ? (
         <div className="generator-options row">
-         <div className="col-md-6 text-center">
-            <label className="custom-label" htmlFor="operationSelect">
+          <div className="col-md-6 text-center">
+            <label className='custom-label'>
               Select Operation:
+              <select
+                className="form-select form-select-lg mb-3 custom-label"
+                value={selectedOperation}
+                onChange={handleOperationChange}
+                aria-label=".form-select-lg"
+                
+               
+              >
+                <option value="+">Addition</option>
+                <option value="-">Subtraction</option>
+                <option value="x">Multiplication</option>
+                <option value="÷">Division</option>
+              </select>
             </label>
-            <select
-              className="custom-select"
-              id="operationSelect"
-              value={selectedOperation}
-              onChange={handleOperationChange}
-              aria-label="Select operation"
-            >
-              <option value="+">Addition</option>
-              <option value="-">Subtraction</option>
-              <option value="x">Multiplication</option>
-              <option value="÷">Division</option>
-            </select>
           </div>
 
           <div className="col-md-6 text-center">
